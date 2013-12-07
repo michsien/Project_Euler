e6014fe86013ac7272450ad576f1df2fc6410d78
Michal Sienkiewicz <mich.sienkiewicz@gazeta.pl>
2013-12-07 03:53:15 +0000

    Problem 18

--- Problems11_20.java
+++ Problems11_20.java
@@ -314,9 +314,35 @@ public class Problems11_20{
     System.out.println(36 * 190 + 70 * 10 + 46 * 100 + 7 * 900 + 3 * 9 * 99 + 11);
   }
 
-// Problem 18 :
+// Problem 18 : Find route from top to bottom of given trinagle (moving to 
+// adjacent numbers on the row below) with maximum total
 
   public static void problem18() {
+    int[][] triangle = {
+      {75},
+      {95, 64},
+      {17, 47, 82},
+      {18, 35, 87, 10},
+      {20, 4, 82, 47, 65},
+      {19, 1, 23, 75, 3, 34},
+      {88, 2, 77, 73, 7, 63, 67},
+      {99, 65, 4, 28, 6, 16, 70, 92},
+      {41, 41, 26, 56, 83, 40, 80, 70, 33},
+      {41, 48, 72, 33, 47, 32, 37, 16, 94, 29},
+      {53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14},
+      {70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57},
+      {91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48},
+      {63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31},
+      {4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23}
+    };
+    //Dynamically calculate the routes with max total starting at the bottom of
+    //the triangle
+    for (int i = triangle.length - 2; i >= 0; i--) {
+      for (int j = 0; j < triangle[i].length; j++) {
+        triangle[i][j] += Math.max(triangle[i + 1][j], triangle[i + 1][j + 1]);
+      }
+    };
+    System.out.println(triangle[0][0]);
   }
 
 // Problem 19 :
